60518fc267b8b8c0098b6e7e1678c2a5d4235628
Eugene Shtoka <eshtoka@gmail.com>
2023-12-11 15:48:17 +0200

    update

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -3,9 +3,12 @@ sudo apt install git -y
 
 rm -rf .git
 rm -rf .zshfn
+rm -rf .scripts
 rm .zshrc
 rm .config/geary/user-style.css
-rm install.sh
+rm .gitignore
+rm LICENSE
+rm README.md
 
 git init
 git branch -m main
@@ -14,13 +17,19 @@ git pull git@github.com:EugeneShtoka/ubuntu-install.git main
 git fetch
 git branch main --set-upstream-to origin/main
 
-rm install.sh
 rm LICENSE
 rm README.md
+git update-index --assume-unchanged README.md
+git update-index --assume-unchanged LICENSE
 
 sudo apt install curl -y
 sudo apt install zsh -y
 chsh -s $(which zsh)
 
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+bash ~/.scripts/gnome-plugins.sh
