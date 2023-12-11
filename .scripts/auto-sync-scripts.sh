7f44bd772b180bad3bc3c7fa7766dea750e6edcf
Eugene Shtoka <eshtoka@gmail.com>
2023-12-11 17:53:32 +0200

    update

--- .scripts/auto-sync-scripts.sh
+++ .scripts/auto-sync-scripts.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Specify the folder to monitor
-TARGET_FOLDER=~/.zshfn
-
-# Specify the script to execute on changes
-ACTION_SCRIPT=/path/to/your/script.sh
-
 # Start monitoring the folder
-inotifywait -q -r -m -e modify,create,delete "$TARGET_FOLDER" | while read -r event; do
+inotifywait -q -r -e DELETE,CLOSE_WRITE ~/.zshrc ~/.zshfn ~/.scripts | while read -r dir event filename; do
   # Execute the action script
   # "${ACTION_SCRIPT}"
-  echo $event
+  echo $event $dir$filename
 done
+
+bash ~/.scripts/auto-sync-scripts.sh
