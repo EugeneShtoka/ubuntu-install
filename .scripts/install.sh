4bc54899fb2d2e3fc6f8e3f3450ca2dc468d1b77
Eugene Shtoka <eshtoka@gmail.com>
2023-12-14 00:09:55 +0200

    update

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -34,4 +34,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.
 rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-bash ~/.scripts/gnome-plugins.sh
+bash ~/.scripts/install-gnome-plugins.sh
