5066331851e8eedb9474fa40fa420003abfdc86a
Eugene Shtoka <eshtoka@gmail.com>
2023-12-14 00:16:21 +0200

    update

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -35,4 +35,3 @@ rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 bash ~/.scripts/gnome-plugins-install.sh
-bash ~/.scripts/dot-net-install.sh
