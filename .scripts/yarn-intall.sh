1adbdeaca7e7be7ac93a9f059f88af2ed834914c
Eugene Shtoka <eshtoka@gmail.com>
2023-12-14 00:18:08 +0200

    update

--- .scripts/yarn-intall.sh
+++ .scripts/yarn-intall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/yarn.gpg
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/yarn.gpg] https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
