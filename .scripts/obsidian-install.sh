5c44aaef9efa3b2221d8d37a82dcd896f0afb24a
Eugene Shtoka <eshtoka@gmail.com>
2023-12-14 00:07:37 +0200

    update

--- .scripts/obsidian-install.sh
+++ .scripts/obsidian-install.sh
@@ -1,5 +1,7 @@
-  curl -L https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/obsidian_1.4.16_amd64.snap -o obsidian.snap
-  snap install obsidian.snap --dangerous --classic
-  rm obsidian.snap
-  rm ~/Documents/Obsidian
-  git clone git@github.com:EugeneShtoka/Obsidian.git ~/Documents/Obsidian
+#!/bin/bash
+
+curl -L https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/obsidian_1.4.16_amd64.snap -o obsidian.snap
+snap install obsidian.snap --dangerous --classic
+rm obsidian.snap
+rm ~/Documents/Obsidian
+git clone git@github.com:EugeneShtoka/Obsidian.git ~/Documents/Obsidian
