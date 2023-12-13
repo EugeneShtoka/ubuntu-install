dc9dba032448f4ed95ea3711bac1497eb7376134
Eugene Shtoka <eshtoka@gmail.com>
2023-12-13 22:19:16 +0200

    update

--- .scripts/pull-custom-scripts.sh
+++ .scripts/pull-custom-scripts.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 cd ~
-rm -rf .zshfn
-rm -rf .scripts
-rm .zshrc
-rm .config/geary/user-style.css
-rm LICENSE
-rm README.md
 git reset --hard
 git pull git@github.com:EugeneShtoka/ubuntu-install.git main
 rm LICENSE
