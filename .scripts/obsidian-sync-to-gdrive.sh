a9e5bc3f81a692000cd734e17909ab4700a5dc56
Eugene Shtoka <eshtoka@gmail.com>
2023-12-13 22:01:29 +0200

    update

--- .scripts/obsidian-sync-to-gdrive.sh
+++ .scripts/obsidian-sync-to-gdrive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rclone sync ~/Documents/Obsidian private:Eugene/Obsidian --exclude ~/Documents/Obsidian/.git --exclude ~/Documents/Obsidian/.trash --exclude ~/Documents/Obsidian/.gitignore
+rclone sync ~/Documents/Obsidian private:Eugene/Obsidian --exclude ~/Documents/Obsidian/.git/** --exclude ~/Documents/Obsidian/.trash/** --exclude ~/Documents/Obsidian/.gitignore
