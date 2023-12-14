47f3ed097bdf85dcc792820f80e1de6ba7fcd908
Eugene Shtoka <eshtoka@gmail.com>
2023-12-14 11:32:30 +0200

    update

--- .scripts/custom-scripts-pull.sh
+++ .scripts/custom-scripts-pull.sh
@@ -3,7 +3,5 @@
 cd ~
 git reset --hard
 git pull git@github.com:EugeneShtoka/ubuntu-install.git main
-rm LICENSE
-rm README.md
-git update-index --assume-unchanged README.md
-git update-index --assume-unchanged LICENSE
+
+bash ~/.scripts/custom-scripts-delete-license-and-readme.sh
