f7bb69785892f4d7e1a19b61c63107dd5363c9f5
Eugene Shtoka <eshtoka@gmail.com>
2023-12-15 15:46:27 +0200

    update

--- .scripts/app-install.sh
+++ .scripts/app-install.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
 sudo apt install curl -y
-
 sudo apt install geary -y
-
 sudo apt install inotify-tools -y
-
 sudo apt install qbittorrent -y
-
 sudo apt install gh -y
-
 sudo apt install glab -y
-
 sudo apt install sxhkd -y
-
 sudo apt install xdotool -y
 
 sudo snap install slack
-
 sudo snap install morgen
-
 sudo snap install atom --classic
 
 gh auth login
 
 bash ~/.scripts/obsidian-install.sh
-
 bash ~/.scripts/docker-install.sh
-
 bash ~/.scripts/vs-code-install.sh
 
